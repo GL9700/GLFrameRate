528dd570ea81dc86f58811a1008acc0219f661e2
LiGuoliang <>
2021-11-16 13:53:00 +0800

    增加滚动列表 用来测试 13pro的高刷

--- Example/GLFrameRate/GLViewController.m
+++ Example/GLFrameRate/GLViewController.m
@@ -8,22 +8,36 @@
 
 #import "GLViewController.h"
 
-@interface GLViewController ()
-
+@interface GLViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic) UITableView *tv;
 @end
 
 @implementation GLViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.tv.frame = self.view.bounds;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1000;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL"];
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"CELL"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"row-%ld", indexPath.row];
+    return cell;
 }
 
+- (UITableView *)tv {
+    if(!_tv) {
+        _tv = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tv.delegate = self;
+        _tv.dataSource = self;
+        [self.view addSubview:_tv];
+    }
+    return _tv;
+}
 @end
